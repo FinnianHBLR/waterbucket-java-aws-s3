24ec00b6e64d8d0f288680c152111891ad79f1a9
FinnianLR <finnniansproject@gmail.com>
2022-12-14 18:28:13 +1300

    Removed old code, added BucketManager class. Added README.md. Added download from bucket.

--- src/main/java/org/finnhblr/jaws/Main.java
+++ src/main/java/org/finnhblr/jaws/Main.java
@@ -1,22 +1,17 @@
 package org.finnhblr.jaws;
 import org.finnhblr.jaws.dataManager.BucketManager;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
-import com.amazonaws.services.s3.model.Bucket;
-
-import java.util.List;
-
-
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("lo");
-        // Create new bucket
         BucketManager s3Manager = new BucketManager();
+        // Create new bucket
         s3Manager.createBucket("shepp-waterbucket");
+        // Upload file to bucket
+        s3Manager.uploadFile("shepp-waterbucket", "bucket.svg", "C:\\Users\\finnn\\Desktop\\Java\\java-aws\\images\\bucket.svg");
+        // Download bucket
+        s3Manager.downloadBucket("shepp-waterbucket", "bucket.svg");
     }
 
-    }
+}
--- src/main/java/org/finnhblr/jaws/dataManager/BucketManager.java
+++ src/main/java/org/finnhblr/jaws/dataManager/BucketManager.java
@@ -4,11 +4,18 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
+import java.io.FileOutputStream;
+
+
+import javax.imageio.ImageIO;
+import java.io.*;
 
 public class BucketManager {
-    public BucketManager(){
-
-    }
+    public BucketManager(){}
 
     public void createBucket(String name) {
         // Init s3 client in Sydney
@@ -28,5 +35,37 @@ public class BucketManager {
             System.out.println(error.getErrorMessage());
         }
 
+
+    }
+    public void uploadFile(String bucketName, String fileName, String path){
+        // fileName is the "key" of the file.
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.AP_SOUTHEAST_2)
+                .build();
+
+        try{
+            s3.putObject(bucketName, fileName, new File(path));
+        } catch (AmazonS3Exception error) {
+            System.err.println(error.getErrorMessage());
+            System.exit(1);
+        }
+    }
+
+    public void downloadBucket(String bucketName, String fileName){
+        // fileName is the "key" of the file.
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.AP_SOUTHEAST_2)
+                .build();
+        // Create request and get data
+        S3Object object = s3.getObject(new GetObjectRequest(bucketName, fileName));
+        S3ObjectInputStream objectContent = object.getObjectContent();
+
+        // Try to save images to ./images/
+        try {
+            IOUtils.copy(objectContent, new FileOutputStream("./images/bucket.svg"));
+        } catch (IOException error) {
+            System.err.println("File saving error!");
+            error.printStackTrace();
+        }
     }
 }
